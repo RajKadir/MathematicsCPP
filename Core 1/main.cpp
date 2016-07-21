f4aa68eeb7760044c021d179a06b1211a36a66a1
Raj Kadir <rajkadir@hotmail.co.uk>
2016-07-21 02:19:17 +0100

    fixed so code will run

--- Core 1/main.cpp
+++ Core 1/main.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 
-void simplifyExpressions()
+void simplifyExpressions_1a()
 {
 
     int x;
     int y;
 
-    // Simplify this expression: 4x - 5y + 3x + 6y
+    // Simplify this expression by collecting like terms:
+    // [1] 4x - 5y + 3x + 6y
     4*x - 5*y + 3*x + 6*y;
 
     // We can simplify this by - rearranging the expression
@@ -21,7 +22,7 @@ void simplifyExpressions()
 int main()
 {
 
-    cout << "Welcome to chapter 1 A Level Mathematics in C++"
+    cout << "Welcome to chapter 1 A Level Mathematics in C++" << endl;
 
     return 0;
 }
