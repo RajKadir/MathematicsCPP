891d5205e2bcc89ba4a6e64c21372a08d989eac9
Raj Kadir <rajkadir@hotmail.co.uk>
2016-07-21 02:14:29 +0100

    Chapter 1 Algebra and Functions, 1.1 Simplifying Expressions

--- Core 1/main.cpp
+++ Core 1/main.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+void simplifyExpressions()
+{
+
+    int x;
+    int y;
+
+    // Simplify this expression: 4x - 5y + 3x + 6y
+    4*x - 5*y + 3*x + 6*y;
+
+    // We can simplify this by - rearranging the expression
+    4*x + 3*x - 5*y + 6*y;
+
+    // Then do simple addition and we get 7x + y
+    7*x + y;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    cout << "Welcome to chapter 1 A Level Mathematics in C++"
+
     return 0;
 }
