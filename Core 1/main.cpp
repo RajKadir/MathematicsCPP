74727b1614366e6179212056fc431274dcf4c97d
Raj Kadir <rajkadir@hotmail.co.uk>
2016-07-27 01:28:24 +0100

    Chapter 1, question 1-3

--- Core 1/main.cpp
+++ Core 1/main.cpp
@@ -17,7 +17,92 @@ void simplifyExpressions_1a()
 
     // Then do simple addition and we get 7x + y
     7*x + y;
+
+
+    int a;
+    int ab;
+    int ac;
+
+    // [4] 3ab - 3ac + 3a - 7ab + 5ac
+    3*ab - 3*ac + 3*a - 7*ab + 5*ac
+
+    // Rearrange
+    3*a + 3*ab - 7*ab - 3*ac + 5*ac
+
+    // Simplified to 3a - 4ab + 2ac
+    3*a - 4*ab + 2*ac
+
+
+
+    // [18] (r² + 3t² + 9) - (2r² + 3t² - 4)
+
+    // First multiply everything inside with whatever is outside
+    // +1(r² + 3t² + 9) -1(2r² + 3t² - 4)
+
+    // r² + 3t² + 9 -2r² -3t² + 4
+
+    // Now rearrange
+    // r² -2r² + 3t² -3t² + 9 + 4
+
+    // Add terms
+    // - r² + 13
+
+    // Rearrange
+    // 13 - r²
 }
+
+void simplifyExpressions_1b(){
+
+
+    // Simplify these expressions where '^' is the power of
+
+    // [1] x^3 * x^4
+
+    // When multiplying similar terms just add the powers
+    // so 3 + 4 = 7
+
+    // The answer is x^7
+
+
+    // [18] 3a^4 * 2a^5 * a^3
+
+
+    // Calculating the coefficient
+    // 3 * 2 * 1 = 6
+
+    // Calculating the powers
+    // a^4 * a^5 * a^3 = 4+5+3 = ^12
+
+    // The answer is 6a^12
+
+
+}
+
+void simplifyExpressions_1c(){
+
+    // Expanding expressions and simplifying where possible
+
+    // 6(4x + 3)
+
+    // Basically we can expand the expression by
+    // multiplying each term inside the bracket with the term outside it
+
+    // So
+    // 6*4x = 24x
+    // 6*3  = 18
+    // = 24x + 18
+
+
+    // Expand and simplify where possible
+    // [20] 3x^2(2x+1) -5x^2(3x-4)
+
+    // 3x^2*2x +3x^2*1 -5x^2*3x -5x^2*-4
+    // = 6x^3 + 3x^2 -15x^3 +20x^2
+    // = 6x^3 -15x^3 + 3x^2 + 20x^2
+    // = -9x^3 + 23x^2
+    // = 23x^2 -9x^3
+}
+
 
 int main()
 {
