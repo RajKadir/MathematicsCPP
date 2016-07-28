fcfebede910b6c59fa5e57dcf710187a2be7be3f
Raj Kadir <rajkadir@hotmail.co.uk>
2016-07-28 09:43:56 +0100

    Made the text more friendly and easier to read

--- Core 1/coordinateGeometry.cpp
+++ Core 1/coordinateGeometry.cpp
@@ -8,4 +8,60 @@ using namespace std;
 
 void coordinateGeometry_5a(){
 
+    cout << "---- Section 5a ----" << endl << endl;
+
+    /*
+        Question 1a
+    */
+    cout << "[1] Find the gradient of these lines" << endl;
+    cout << "\t 1a) y = -2x + 5" << endl;
+    cout << "\t We can find the gradient by 'y = mx + c' where 'm' is the gradient" << endl;
+    cout << "\t The gradient must be -2" << endl << endl;
+    getch();
+
+    /*
+        Question 1h
+    */
+    cout << "\t 1h) 10x - 5y + 1 = 0" << endl;
+    cout << "\t Write the equation in the form of y = mx + c" << endl;
+    cout << "\t 5y = 10x + 1" << endl;
+    cout << "\t y = 10/5x + 1/5" << endl;
+    cout << "\t y = 2x + 1/5" << endl;
+    cout << "\t So the gradient is 2" << endl << endl;
+    getch();
+
+    /*
+        Question 2a
+    */
+    cout << "[2] These lines intercept the y axis at 0, c - find c " << endl << endl;
+
+    cout << "\t 2a) y = -x + 4 " << endl;
+    cout << "\t y = mx + c" << endl;
+    cout << "\t m = -1, c = 4" << endl;
+    cout << "\t So c = 4" << endl << endl;
+    getch();
+
+    /*
+        Question 2h
+    */
+    cout << "\t 2h) 4x -5y - 10 = 0" << endl;
+    cout << "\t Rearrange to y = mx + c" << endl;
+    cout << "\t 5y = 4x - 10" << endl;
+    cout << "\t divide each term by 5" << endl;
+    cout << "\t y = 4/5x - 2" << endl;
+    cout << "\t c = -2" << endl << endl;
+    getch();
+    /*
+        Question 3b
+    */
+    cout << "[3] Write these lines in the form of ax + by + c = 0" << endl << endl;
+
+    cout << "\t 3b) y = 3x - 2" << endl;
+    cout << "\t Simple rearranging, move y to the RHS" << endl;
+    cout << "\t Answer: 3x - y - 2 = 0" << endl;
+    getch();
+
+
 }
+
+
