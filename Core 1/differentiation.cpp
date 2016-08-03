4a0daea438da7835bcfc8510142bf35261a7f7b5
Raj Kadir <rajkadir@hotmail.co.uk>
2016-08-03 20:44:21 +0100

    Chapter 7, section a-c

--- Core 1/differentiation.cpp
+++ Core 1/differentiation.cpp
@@ -6,14 +6,61 @@
 using namespace std;
 
 void differentiation_7a(){
+    cout << "----------------------------------------------------" << endl;
+    cout << "-------------------- Section 7a --------------------" << endl;
+    cout << "----------------------------------------------------" << endl << endl;
+
+
+    cout << "[1] F is the point with co-ordinates (3,9) on curve y=x^2 " << endl;
+    cout << "\t 1a) Find the gradients of the chords joining points:" << endl;
+    cout << "\t i) (4, 16)" << endl;
+    cout << "\t Use y2-y1/x2-x1 = 16-9/4-3 = 7/1 = 7" << endl;
+    cout << "\t Answer: 7 " << endl << endl;
+    getch();
 
 }
 
 void differentiation_7b(){
+    cout << "----------------------------------------------------" << endl;
+    cout << "-------------------- Section 7b --------------------" << endl;
+    cout << "----------------------------------------------------" << endl << endl;
+
+    cout << "Find the derived function, given that f(x) equals" << endl;
+    cout << "\t [1] x^7 " << endl;
+    cout << "\t Let f(x) = x^7" << endl;
+    cout << "\t Answer: f'(x) = 7x^6" << endl;
+    cout << "\t I did this by multiplying x by the power then subtract 1 to the power" << endl << endl;
+    getch();
+
+    cout << "\t [2] x^8 " << endl;
+    cout << "\t Let f(x) = x^8" << endl;
+    cout << "\t Answer: f'(x) = 8x^7" << endl << endl;
+    getch();
+
+    cout << "\t [17] x^2 x x^3 " << endl;
+    cout << "\t Let f(x) = x^5 " << endl;
+    cout << "\t Answer: f'(x) = 5x^4" << endl << endl;
+    getch();
 
 }
 
 void differentiation_7c(){
+    cout << "----------------------------------------------------" << endl;
+    cout << "-------------------- Section 7c --------------------" << endl;
+    cout << "----------------------------------------------------" << endl << endl;
+
+    cout << "[1] Find dy/dx when y equals: " << endl;
+    cout << "\t 1a) 2x^2 - 6x + 3" << endl;
+    cout << "\t dy/dx = (2)2x^2-1 (1)-6x^1-1 + 3(0)" << endl;
+    cout << "\t differentiating a constant gives 0, anything to the power of 0 is 1" << endl;
+    cout << "\t answer: dy/dx = 4x - 6" << endl << endl;
+    getch();
+
+    cout << "[2] Find the gradient of the curve whose equation is" << endl;
+    cout << "\t 2a) y = 3x^2 at point (2,12)" << endl;
+    cout << "\t dy/dx = 6x, m = 6x" << endl;
+    cout << "\t gradient = 6(2), where x = 2" << endl;
+    cout << "\t Answer: 12" << endl;
 
 }
 
