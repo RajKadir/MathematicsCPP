cd26e6f0e41cddbae3f016522bde597a9806b83b
Raj Kadir <rajkadir@hotmail.co.uk>
2016-08-03 19:23:19 +0100

    Chapter 6 Section C-F

--- Core 1/sequenceAndSeries.cpp
+++ Core 1/sequenceAndSeries.cpp
@@ -57,21 +57,149 @@ void sequenceAndSeries_6b(){
     cout << "\t n = 14" << endl << endl;
     getch();
 
-    cout << "\t 2d) Un = 2n + 1/ n-3, Un = 19/6" << endl;
+    cout << "\t 2d) Un = 2n + 1/ n-3, Un = 19/6" << endl << endl;
 
 
 }
 
 void sequenceAndSeries_6c(){
+    cout << "----------------------------------------------------" << endl;
+    cout << "-------------------- Section 6c --------------------" << endl;
+    cout << "----------------------------------------------------" << endl << endl;
+
+    /*
+        Question 1a
+    */
+    cout << "[1] Find the first four terms of the following recurrence relationships:" << endl;
+    cout << "\t 1a) Un+1 = Un + 3, U1 = 1" << endl;
+    cout << "\t First term:         U1 = 1" << endl;
+    cout << "\t Substituting n = 1, U2 = U1 + 3 = 1 + 3 = 4" << endl;
+    cout << "\t Substituting n = 2, U3 = U2 + 3 = 4 + 3 = 7" << endl;
+    cout << "\t Substituting n = 3, U4 = U3 + 3 = 7 + 3 = 10" << endl;
+    cout << "\t Answer: 1, 4, 7, 10" << endl << endl;
+    getch();
+
+
+    /*
+        Question 1f
+    */
+    cout << "\t 1f) Un+1 = (Un)^2 - 1, U1 = 2" << endl;
+    cout << "\t First term:       U1 = 2" << endl;
+    cout << "\t Substitute n = 1, U2 = (U1)^2 - 1 = (2)^2 - 1 = 4 - 1 = 3 " << endl;
+    cout << "\t Substitute n = 2, U3 = (U2)^2 - 1 = (3)^2 - 1 = 9 - 1 = 8 " << endl;
+    cout << "\t Substitute n = 3, U4 = (U3)^2 - 1 = (8)^2 - 1 = 63" << endl;
+    cout << "\t Answer: 2, 3, 8, 63" << endl << endl;
+    getch();
+
+
+    /*
+        Question 2a
+    */
+    cout << "[2] Suggest possible recurrence relationships with the following sequences:" << endl;
+    cout << "\t 2a) 3, 5, 7, 9" << endl;
+    cout << "\t We can describe it as adding 2 to the previous term. " << endl;
+    cout << "\t Answer: Uk+1 = Uk + 2, U1 = 3" << endl << endl;
+    getch();
+
+
+    /*
+        Question 2j
+    */
+    cout << "\t 2j) 4, 10, 18, 38, 74" << endl;
+    cout << "\t We can describe it as the previous term + 2(term before that)" << endl;
+    cout << "\t So 10 + 2(4) = 18, 18 + 2(10) = 38, 38 + 2(18) = 74" << endl;
+    cout << "\t So we have Un = Un-1 + 2(Un-2)" << endl;
+    cout << "\t But that needs to be corrected in the form Uk+1 = Uk " << endl;
+    cout << "\t Answer: Un+2 = Un+1 + 2Un, U1 =4, U2 = 10" << endl << endl;
+    getch();
+
+
 }
 
 void sequenceAndSeries_6d(){
+    cout << "----------------------------------------------------" << endl;
+    cout << "-------------------- Section 6d --------------------" << endl;
+    cout << "----------------------------------------------------" << endl << endl;
+
+    /*
+        Question 1a
+    */
+    cout << "[1] Which of the following sequences are arithmetic? " << endl;
+    cout << "\t A sequence in the form of 'Uk+1 = Uk + n' is arithmetic." << endl << endl;
+
+    cout << "\t 1a) 3, 5, 7, 9, 11" << endl;
+    cout << "\t the sequence is adding 2 every time " << endl;
+    cout << "\t Uk+1 = Uk + 2" << endl;
+    cout << "\t Answer: the sequence is arithmetic." << endl << endl;
+    getch();
+
+    cout << "\t 1b) 10, 7, 4, 1" << endl;
+    cout << "\t We are taking away -3 every time" << endl;
+    cout << "\t this could be written as Uk+1 = Uk + -3" << endl;
+    cout << "\t Answer: the sequence is arithmetic." << endl << endl;
+    getch();
+
+    cout << "\t 1d) 1, 4, 9, 16, 25" << endl;
+    cout << "\t We are squaring each number 1, 2, 3, 4, 5" << endl;
+    cout << "\t This sequence could be written as: Un = n^2" << endl;
+    cout << "\t Answer: the sequence is not arithmetic. " << endl << endl;
+    getch();
+
+
+    cout << "[2] Find the 10th and nth terms in the following arithmetic progressions: " << endl;
+    cout << "\t 2a) 5, 7, 9, 11" << endl;
+    cout << "\t add 2 every term, so 5 + 9x2 = 23" << endl;
+    cout << "\t Answer: 23" << endl << endl;
+    cout << "\t Alternative approach: " << endl;
+    cout << "\t use 'a + (n-1)d' where a is the first term, d is the common difference" << endl;
+    cout << "\t 5 + (10-1)(2) = 5 + (9)(2) = 5 + 18 = 23" << endl;
+    cout << "\t Answer: U10 = 23" << endl << endl;
+    getch();
+
+
+
+
 }
 
 void sequenceAndSeries_6e(){
+    cout << "----------------------------------------------------" << endl;
+    cout << "-------------------- Section 6e --------------------" << endl;
+    cout << "----------------------------------------------------" << endl << endl;
+
+    /*
+        Question 2a
+    */
+    cout << "[1] Find the 20th and nth term of the arithmetic sequences " << endl;
+    cout << "\t 1a) 2 + 6 + 10 + 14 + 18" << endl;
+    cout << "\t 2 + (19)(4) = 78" << endl;
+    cout << "\t 2 + (n-1)(4) = 2 + 4n -4 = 4n - 2" << endl;
+    cout << "\t Answer: 20th term = 78, nth term = 4n - 2" << endl << endl;
+    getch();
+
 }
 
 void sequenceAndSeries_6f(){
+    cout << "----------------------------------------------------" << endl;
+    cout << "-------------------- Section 6f --------------------" << endl;
+    cout << "----------------------------------------------------" << endl << endl;
+
+    cout << "Finding the sum of an arithmetic sequence." << endl;
+    cout << "\t Use Sn = n/2[2a + (n-1)d]" << endl;
+    cout << "\t there are n lots of 2a +(n-1)d" << endl;
+    cout << "\t or use Sn = n/2(a+L) where L is last term" << endl << endl;
+    getch();
+
+    /*
+        Question 1a
+    */
+    cout << "[1] Find the sum of the following series:" << endl;
+    cout << "\t 1a) 3 + 7 + 11 + 14 + ... (20 terms)" << endl;
+    cout << "\t 20/2[2(3)+(20-1)(4)] = 10[6 + (19)(4)] = 10(6 + 76) = 10(82) = 820" << endl;
+    cout << "\t Answer: 820" << endl << endl;
+    getch();
+
+
+
 }
 
 
