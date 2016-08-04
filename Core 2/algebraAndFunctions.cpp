b65b3df91e85dafa0a3dd3eb0ebb591d5cdad507
Raj Kadir <rajkadir@hotmail.co.uk>
2016-08-04 23:10:30 +0100

    Chapter 1: Algebra and functions, Section A, Question 1-2

--- Core 2/algebraAndFunctions.cpp
+++ Core 2/algebraAndFunctions.cpp
@@ -4,6 +4,49 @@ using namespace std;
 
 void algebraAndFunctions_1a(){
 
+    cout << "----------------------------------------------------" << endl;
+    cout << "-------------------- Section 1a --------------------" << endl;
+    cout << "----------------------------------------------------" << endl << endl;
+
+    cout << "[1] Simplify these fractions: " << endl;
+    cout << "\t 1a) (4x^4 + 5x^2 - 7x)/x" << endl;
+    cout << "\t Divide each term: 4x^4/x + 5x^2/x -7x/x" << endl;
+    cout << "\t Rules of Indices: 4x^3 + 5x -7x^0" << endl;
+    cout << "\t Answer: 4x^3 + 5x -7" << endl << endl;
+    getch();
+
+    cout << "\t 1l) (-9x^9 -6x^4 - 2)/-3x" << endl;
+    cout << "\t Divide each term: -9x^9/-3x -6x^4/3x -2/-3x" << endl;
+    cout << "\t Rules of Indices: +3x^8 +2x^3 + 2/3x^-1"
+    cout << "\t Answer: 3x^8 + 2x^3 + 2/3x^-1" << endl << endl;
+    getch();
+
+
+    cout << "[2] Simply these fractions as far as possible: " << endl;
+    cout << "\t 1a) (x+3)(x-2)/(x-2)" << endl;
+    cout << "\t we can divide out (x-2), so we are left with (x+3)" << endl;
+    cout << "\t Answer: x + 3" << endl << endl;
+    getch();
+
+    cout << "\t 1o) (2x^2-5x-3)/(2x^2-9x+9)" << endl;
+    cout << "\t Factorise: 2x^2 -5x -3 " << endl;
+    cout << "\t 2 * -3 = -6" << endl;
+    cout << "\t and -6 * 1 = -6" << endl;
+    cout << "\t     -6 + 1 = -5" << endl;
+    cout << "\t So 2x^2 -5x -3 = 2x^2 -6x + 1x -3" << endl;
+    cout << "\t = 2x(x-3)1(x-3)" << endl;
+    cout << "\t = (2x+1)(x-3)" << endl;
+    cout << "\t Factorise: 2x^2-9x+9" << endl;
+    cout << "\t 2 * 9 = 18" << endl;
+    cout << "\t and  -6 * -3 = 18" << endl;
+    cout << "\t      -6 + -3 = -9" << endl;
+    cout << "\t So 2x^2 -9x +9 = 2x^2 -6x -3x + 9" << endl;
+    cout << "\t = 2x(x-3)-3(x-3)" << endl;
+    cout << "\t = (2x-3)(x-3)" << endl;
+    cout << "\t = (2x+1)(x-3)/(2x-3)(x-3)" << endl;
+    cout << "\t divide out (x-3)" << endl;
+    cout << "\t Answer: (2x+1)/(2x-3)" << endl << endl;
+    getch();
 }
 
 void algebraAndFunctions_1b(){
