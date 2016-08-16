15a83a948fa9e5cfbef30eb9a8e27e431474c6fb
Raj Kadir <rajkadir@hotmail.co.uk>
2016-08-16 23:39:44 +0100

    Added mechanics 1, and core 1 dependencies file

---
+++ Mechanics 1/Mechanics 1/kinematicsOfAParticleMovingInAStraightLine.h
@@ -0,0 +1,5 @@
+void kinematicsOfAParticleMovingInAStraightLine_2a();
+void kinematicsOfAParticleMovingInAStraightLine_2b();
+void kinematicsOfAParticleMovingInAStraightLine_2c();
+void kinematicsOfAParticleMovingInAStraightLine_2d();
+void kinematicsOfAParticleMovingInAStraightLine_summary();
