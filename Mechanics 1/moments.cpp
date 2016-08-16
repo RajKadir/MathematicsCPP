c57e6cf34891e8f4d107e94cad4270472b015a12
Raj Kadir <rajkadir@hotmail.co.uk>
2016-08-16 23:44:50 +0100

    Add files via upload

---
+++ Mechanics 1/moments.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+#include "moments.h"
+using namespace std;
+
+void moments_5a(){
+
+}
+
+void moments_5b(){
+
+}
+
+void moments_5c(){
+
+}
+
+void moments_5d(){
+
+}
+
+void moments_summary(){
+
+}
---
+++ Mechanics 1/staticsOfAParticle.h
@@ -0,0 +1,4 @@
+void staticsOfAParticle_4a();
+void staticsOfAParticle_4b();
+void staticsOfAParticle_4c();
+void staticsOfAParticle_summary();
